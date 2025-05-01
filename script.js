2005974e53163f5d13a89a4e3694a2d6507dfbd7
swethamkulkarni <smk10032002@gmail.com>
2025-05-02 00:30:06 +0100

    updated css and script

--- script.js
+++ script.js
@@ -2,18 +2,18 @@ document.addEventListener("DOMContentLoaded", function () {
     const links = document.querySelectorAll("nav ul li a");
   
     links.forEach(link => {
-      link.addEventListener("click", function (e) {
-        e.preventDefault(); // stop default nav for now
-        const destination = this.href;
-  
-        // Add fade-out class to trigger CSS transition
-        document.body.classList.add("fade-out");
-  
-        // After animation ends, go to the new page
-        setTimeout(() => {
-          window.location.href = destination;
-        }, 500); // match this to the transition duration in CSS
+        link.addEventListener("click", function (e) {
+          e.preventDefault(); // stop default nav for now
+          const destination = this.href;
+    
+          // Add fade-out class to trigger CSS transition
+          document.body.classList.add("fade-out");
+    
+          // After animation ends, go to the new page
+          setTimeout(() => {
+            window.location.href = destination;
+          }, 500); // match this to the transition duration in CSS
+        });
       });
-    });
   });
   
